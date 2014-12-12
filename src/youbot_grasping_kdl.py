8565e507b458a30bedebb4a95eca3c27785dc4c9
youBot Master <youbot@ubuntu.com>
2014-12-12 15:10:53 -0600

    The arm now goes through a routine of moving above object, moving down to grasp, and then going back to candle position.  The code is pretty ugly right now, but it functions.  Clean up comes later.

--- src/youbot_grasping_kdl.py
+++ src/youbot_grasping_kdl.py
@@ -3,9 +3,10 @@
 
 import rospy
 import numpy as np
-import scipy.linalg
+#import scipy.linalg
 import math
 import tf
+import tf.transformations as trans
 
 from tf.transformations import euler_from_quaternion
 from tf.transformations import compose_matrix
@@ -28,46 +29,117 @@ jointObject= [3.04171,0.63597,-1.017845,0.36284,2.876194]
 jointGrasp = [3.04171,2.04427,-1.5189129,2.5434289757,2.8761944]
 jointInitialize= [0.01007,.635971,-1.91989,1.04424,2.87619]
 
-jointGuessForGrasp=[np.pi + 1.39586, 0.368976, -3.54202, 0.9626105, 2.89177]
+jointGuessForGrasp=[0.0, 0.0, 1.52, 1.84, -1.26, 2.4, 3.10]
+#jointGuessForGrasp=[np.pi + 1.39586, 0.368976, -3.54202, 0.9626105, 2.89177]
 
+armJointPosCandle = np.array([2.9496, 1.1344, -2.5482, 1.789, 2.9234])
+
+
+def matmult(*x):
+  return reduce(np.dot, x)
 
 def hat(w):
-	return np.array([[0,-w[2],w[1]],
-			 [w[2],0,-w[0]],
-			 [-w[1],w[0],0]])
+    return np.array([[0,-w[2],w[1]],
+		     [w[2],0,-w[0]],
+		     [-w[1],w[0],0]])
 
 def expm(w,th):
-	return np.identity(3) + hat(w)*np.sin(th) + \
-		 np.dot(hat(w),hat(w))*(1-np.cos(th))
+    return np.identity(3) + hat(w)*np.sin(th) + np.dot(hat(w),hat(w))*(1-np.cos(th))
 
 def quat_to_so3(quat):
-	try:
-		q = np.array([quat.x,quat.y,quat.z])
-		th = quat.w
-	except AttributeError:
-		error1 = True
-		pass
-	try: 
-		q = np.array(quat[0:3])
-		th = quat[3]
-	except IndexError:
-		error2 = True
-		pass
-	if error1 or error2:
-		# rospy.logerr("Invalid data sent to quat_to_se3")
-		print "Error"
-	
-	th = 2*np.arccos(th)
-	if th != 0:
-		q /= np.sin(th/2.0)
-	else:
-		q *= 0.0
-	return scipy.linalg.expm(hat(q)*th)
+    q = np.array(quat[0:3])
+    th = quat[3]
+    th = 2*np.arccos(th)
+    if th != 0:
+	q /= np.sin(th/2.0)
+    else:
+	q *= 0.0
+    # return scipy.linalg.expm(hat(q)*th)
+    return expm(q,th)
 
 def quat_pos_to_se3(quat,pos):
-	R = quat_to_so3(quat)
-	g = np.hstack((R, np.array([pos.ravel()]).T))
-	return np.vstack((g,[0,0,0,1]))
+    R = quat_to_so3(quat)
+    g = np.hstack((R, np.array([pos.ravel()]).T))
+    return np.vstack((g,[0,0,0,1]))
+
+
+def dls_ik(kin, pose, q0, lam=0.25, num=100):
+    # get position and orientation:
+    Rd = pose[0:3,0:3]
+    Pd = pose[0:3,-1]
+    # setup iterations:
+    q = q0.copy()
+    # run loop trying to get to target:
+    for i in range(num):
+	J = kin.jacobian(q)
+	g = np.array(kin.forward(q))
+	R = g[0:3,0:3]
+	P = g[0:3,-1]
+	Rdelt = matmult(Rd, R.T)
+	rdelt_angles = np.array(trans.euler_from_matrix(Rdelt))
+	e = np.hstack((Pd-P, rdelt_angles))
+	dq = np.array(matmult(J.T,np.linalg.inv(matmult(J,J.T) + lam*np.eye(J.shape[0]))))
+	q += matmult(dq,e)
+	##############################################################
+	# should add a break condition and corresponding tolerance!! #
+	##############################################################
+    return q
+
+
+
+def dls_ik_position_only(kin, pose, q0, lam=0.25, num=100):
+    # get position and orientation:
+    Rd = pose[0:3,0:3]
+    Pd = pose[0:3,-1].ravel()
+    # setup iterations:
+    q = q0.copy()
+    # run loop trying to get to target:
+    for i in range(num):
+	J = kin.jacobian(q)
+	J = J[0:3,:]
+	g = np.array(kin.forward(q))
+	P = g[0:3,-1]
+	e = Pd-P.ravel()
+	dq = np.array(matmult(J.T,np.linalg.inv(matmult(J,J.T) + lam*np.eye(J.shape[0]))))
+	q += matmult(dq,e)
+	##############################################################
+	# should add a break condition and corresponding tolerance!! #
+	##############################################################
+    return q
+
+
+##
+# Performs an IK search while trying to balance the demands of reaching the goal,
+# maintaining a posture, and prioritizing rotation or position.
+def inverse_biased(kin, pose, q_init, q_bias, q_bias_weights, rot_weight=1.0, 
+		   bias_vel=0.01, num_iter=100):
+    # This code is potentially volatile
+    q_out = q_init.copy()
+    pos = pose[0:3,-1]
+    rot = pose[0:3,0:3]
+    for i in range(num_iter):
+	# pos_fk, rot_fk = PoseConv.to_pos_rot(self.forward(q_out))
+	g = np.array(kin.forward(q_out))
+	pos_fk = g[0:3,-1]
+	rot_fk = g[0:3,0:3]
+	delta_twist = np.array(np.zeros(6))
+	pos_delta = pos - pos_fk
+	delta_twist[:3] = pos_delta
+	rot_delta = np.eye(4)
+	rot_delta[:3,:3] = rot * rot_fk.T
+	rot_delta_angles = np.array(trans.euler_from_matrix(rot_delta))
+	delta_twist[3:6] = rot_delta_angles
+	J = np.array(kin.jacobian(q_out))
+	J[3:6,:] *= np.sqrt(rot_weight)
+	delta_twist[3:6] *= np.sqrt(rot_weight)
+	J_tinv = matmult(np.linalg.inv(matmult(J.T,J) + np.diag(q_bias_weights)), J.T)
+	q_bias_diff = q_bias - q_out
+	q_bias_diff_normed = q_bias_diff * bias_vel / np.linalg.norm(q_bias_diff)
+	delta_q = q_bias_diff_normed + matmult(J_tinv, (delta_twist - matmult(J, q_bias_diff_normed)))
+	q_out += delta_q 
+	q_out = np.array(kin.clip_joints_safe(q_out))
+    return q_out
+
 
 
 def low_high_limit(value, low_limit, high_limit):
@@ -90,7 +162,7 @@ class YoubotArm:
 		self.moving_to_new_x_pos = False
 		self.moving_to_new_y_pos = False
 		self.stop_base_movement = False
-
+		
 		self.max_virtual_x_vel = 0.05
 		self.max_virtual_y_vel = 0.05
 
@@ -107,7 +179,7 @@ class YoubotArm:
 
 		self.have_run_once = False
 
-
+		print "About to parse URDF..."
 		self.youbot_urdf = URDF.from_parameter_server()
 		
 
@@ -119,11 +191,20 @@ class YoubotArm:
 		self.arm_joint_pub = rospy.Publisher("arm_1/arm_controller/position_command",JointPositions,queue_size=10)
 		self.vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
+		print "Publishers created"
+
 		rospy.sleep(2)
+
+		# Go back to the candle position.
+		print "Initializing to the candle position..."
+		self.publish_arm_joint_positions(armJointPosCandle)
+		rospy.sleep(3.0)
+
 
 		self.joint_state_sub = rospy.Subscriber("joint_states", JointState, self.get_joint_states)
 
-                self.kdl_kinematics([])
+		self.kdl_kinematics([])
+                self.grasp_routine()
 
 
 	def vel_monitor_timer_callback(self, data):
@@ -176,10 +257,6 @@ class YoubotArm:
 
 	def publish_arm_joint_positions(self, positions):
 
-		print
-		print "About to publish joints"
-		print
-
 		desiredPositions = JointPositions()
 
 		jointCommands = []
@@ -218,67 +295,129 @@ class YoubotArm:
 
 
 	def kdl_kinematics(self, data):
+                pass
 
-		if self.have_run_once:
-			return
 
-		print 
-		print "Current joint angles:"
-		# print data
+        def set_gripper_width(self, width):
+		  
+		desiredPositions = JointPositions()
 
-		# self.q_sensors = list(data)
-		# self.q_sensors.insert(0, 0.0)
-		# self.q_sensors.insert(0, 0.0)
+		jointCommands = []
+
+                joint = JointValue()
+                joint.joint_uri = "gripper_finger_joint_l"
+                joint.unit = "rad"
+                joint.value = width
+                jointCommands.append(joint)
+
+                joint = JointValue()
+                joint.joint_uri = "gripper_finger_joint_r"
+                joint.unit = "rad"
+                joint.value = width
+                jointCommands.append(joint)
+
+		desiredPositions.positions=jointCommands
+
+		self.arm_joint_pub.publish(desiredPositions)
+
+
+	def grasp_routine(self):
 
 		self.youbot_kin = KDLKinematics(self.youbot_urdf, "virtual", "gripper_palm_link")
+		kin_grasp = KDLKinematics(self.youbot_urdf, "base_link", "gripper_palm_link")
 
-		## --- Test --- ##
+		quat_above_grasp = np.array([0.601, 0.591, -0.372, 0.388])
+		pos_above_grasp = np.array([0.181, 0.778, 0.108])
+		r,p,y = trans.euler_from_quaternion(quat_above_grasp, 'sxyz')
+		pose_arm = quat_pos_to_se3(quat_above_grasp, pos_above_grasp)
+                print
+                print "pose_arm type: ",type(pose_arm)
+                print
 
-		# We will get the current pose of the youbot arm and change it just slightly
-		# and then pass it through the IK solver to get the joint angles.  Then we 
-		# will print the joint angles.
+		# --- Get into position for grasp --- #
 
-		# Current pose matrix.
-		# self.pose_arm = self.youbot_kin.forward(self.q_sensors)
-		# print
-		# print "Current pose:"
-		# print self.pose_arm
+		q_guess = np.array(jointGuessForGrasp)
+		q_ik = self.youbot_kin.inverse(pose_arm, [0.0, 0.0, 1.52, 1.84, -1.26, 2.4, 3.10])
 
-		# Try a real pose
-		# self.pose_arm = tf.transformations.compose_matrix(translate=[0.18, -0.3, 0.18], angles=[0.0, -math.pi * 0.75, math.pi * 0.5])
-		quat = np.array([0.601, 0.591, -0.372, 0.388])
-		pos = np.array([0.181, 0.778, 0.108])
-		self.pose_arm = quat_pos_to_se3(quat, pos)
+		# Try to solve IK for the pickup point.
+		if q_ik != None:
+                        q_ik = np.array(q_ik)
 
-		'''
-		self.pose_arm = tf.transformations.compose_matrix(angles=[0.0, 0.0, -math.pi * 0.5])
-		rot_y_arm = tf.transformations.compose_matrix(angles=[0.0, -math.pi * 0.75, 0.0])
-		self.pose_arm = np.dot(self.pose_arm, rot_y_arm)
-		rot_z_arm = tf.transformations.compose_matrix(angles=[0, 0, math.pi])
-		self.pose_arm = np.dot(self.pose_arm, rot_z_arm)
-		trans_mat = tf.transformations.compose_matrix(translate=[0.18, -0.3, 0.10])
-		self.pose_arm = np.dot(trans_mat, self.pose_arm)
-		'''
-		print
-		print "Target pose:"
-		print self.pose_arm
+			rospy.loginfo("Valid IK found")
+			rospy.loginfo("q =")
+			print q_ik, "\r\n"
 
-		#self.q_guess = np.array(self.q_sensors) + 0.2
-		self.q_guess = np.array(jointGuessForGrasp)
-		self.q_ik = self.youbot_kin.inverse(self.pose_arm, [0.0, 0.0, 1.52, 1.84, -1.26, 2.4, 3.10])
-                print "done with IK"
-
-		# if self.q_ik == None:
-		# 	print
-		# 	print "IK failed, trying IK search..."
-		# 	self.q_ik = self.youbot_kin.inverse(self.pose_arm)
-
-		if self.q_ik != None:
+			# Publish the IK results.
+			arm_joints = [q_ik[2], q_ik[3], q_ik[4], q_ik[5], q_ik[6]]
+			arm_joints = self.limit_arm_joints(arm_joints)
 			print
-			print "Inverse kinematics results:"
-			print self.q_ik
+			print "Publishing..."
+			self.publish_arm_joint_positions(arm_joints)
+
+			# Sleep for a while to give the arm time to get there.
+			rospy.sleep(5.0)
+
+
+                        # --- Open grippers --- #
+                        
+                        print
+                        print "Opening grippers..."
+                        self.set_gripper_width(0.008)
+                        rospy.sleep(1.0)
+
+
+			# --- Move down to grasp --- #
+			
+			# solve IK for the grasp point:
+			print
+			print "Solving for moving down to grasp..."
+			pos_at_grasp = pos_above_grasp.copy()
+                        print
+                        print "pos_at_grasp"
+                        print pos_at_grasp
+			pos_at_grasp[0:2] -= q_ik[0:2]
+			pos_at_grasp[2] -= 0.05 # move down 5 cm
+                        print
+			print "Quaternion"
+			print quat_above_grasp
+                        print
+			print "pos_at_grasp"
+			print pos_at_grasp
+                        print
+			pose_grasp = quat_pos_to_se3(quat_above_grasp, pos_at_grasp)
+			# q_grasp = dls_ik_position_only(kin_grasp, pose_grasp, q_ik[2:], num=10)
+			q_grasp = dls_ik(kin_grasp, pose_grasp, q_ik[2:])
+			# q_init = kin_grasp.clip_joints_safe(np.random.random(5)*0.0005 + q_ik[2:])
+			# q_grasp = inverse_biased(kin_grasp, pose_grasp, q_init, q_ik[2:], np.array([10000, 10000, 10000, 10000, 10000]),
+			#			   bias_vel=5, rot_weight=0.001, num_iter=500)
+			# send arm to pickup point
+			print "Valid grasp point IK found"
+			print "q = "
+			print q_grasp
 			print
 
+			q_grasp = self.limit_arm_joints(q_grasp)
+			self.publish_arm_joint_positions(q_grasp)
+
+			rospy.sleep(5.0)
+
+			# --- Close the grippers --- #
+			
+                        print
+                        print "Closing grippers..."
+                        self.set_gripper_width(0.00471)
+                        rospy.sleep(1.0)
+                        
+
+			# --- Go to the carry position --- #
+
+			# Go back to the candle position.
+			print "Going back to the candle position..."
+			print
+			self.publish_arm_joint_positions(armJointPosCandle)
+			rospy.sleep(5.0)
+
+			'''
 			# Get the position to move the virtual joints to.
 
 			self.commanded_virtual_x_pos = self.q_ik[0]
@@ -310,11 +449,12 @@ class YoubotArm:
 
 			arm_joints = [self.q_ik[2], self.q_ik[3], self.q_ik[4], self.q_ik[5], self.q_ik[6]]
 			arm_joints = self.limit_arm_joints(arm_joints)
-			#self.publish_arm_joint_positions(arm_joints)
+			self.publish_arm_joint_positions(arm_joints)
 
 			self.have_run_once = True
+			'''
 		else:
-			print "Error in the inverse kinematics!"
+			rospy.logwarn("Invalid IK solution!")
 
 
 	def limit_arm_joints(self, joints):
